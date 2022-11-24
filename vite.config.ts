00395cc6388272ed89fd0911abd254cae8f8979f
Girish21 <girishkumarv21@gmail.com>
2022-11-24 23:55:07 +0530

    add RTL 🧪

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,12 @@
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setup.ts'],
+    setupFiles: ['./src/setupTest.ts'],
     testTimeout: 20000,
   },
 })
