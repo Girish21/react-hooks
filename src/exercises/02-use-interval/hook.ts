c48c8102f01e8cbebea68f7f8fb672550ee29cac
SushanthK07 <sushanthkummitha@gmail.com>
2022-12-05 20:52:28 +0530

    useInterval hook

--- src/exercises/02-use-interval/hook.ts
+++ src/exercises/02-use-interval/hook.ts
@@ -1,1 +1,23 @@
-export const useInterval = () => null
+import * as React from 'react'
+
+export const useInterval = (callback: () => void, delay: number) => {
+  const [time, setTime] = React.useState(delay)
+
+  const callbackRef = React.useRef(callback)
+  const intervalRef = React.useRef<ReturnType<typeof setInterval> | null>(null)
+
+  React.useEffect(() => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current)
+    }
+    setTime(delay)
+  }, [delay])
+
+  React.useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
+
+  React.useEffect(() => {
+    intervalRef.current = setInterval(() => callbackRef.current(), time)
+  }, [callback, time])
+}
