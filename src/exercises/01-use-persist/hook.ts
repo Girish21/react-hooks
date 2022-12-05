f1e0ac4f81f08a6ef25b859ebe38a406eee83700
SushanthK07 <sushanthkummitha@gmail.com>
2022-12-05 19:20:41 +0530

    pass func as initial value :smile:

--- src/exercises/01-use-persist/hook.ts
+++ src/exercises/01-use-persist/hook.ts
@@ -3,27 +3,22 @@ import * as React from 'react'
 export const usePersist = <TValue>(
   key: string,
   value: TValue,
-): [TValue, (value: TValue) => void] => {
-  const [state, setState] = React.useState(value)
-
-  const storeValue = (value: TValue) => {
-    setState(value)
-    localStorage.setItem(key, JSON.stringify(value))
-  }
-
-  React.useEffect(() => {
+): [TValue, React.Dispatch<React.SetStateAction<TValue>>] => {
+  const [state, setState] = React.useState(() => {
     try {
       const storedValue = localStorage.getItem(key)
       if (!storedValue) {
         throw new Error()
       }
-      const parsedValue = JSON.parse(storedValue)
-      setState(parsedValue)
-      localStorage.setItem(key, JSON.stringify(parsedValue))
+      return JSON.parse(storedValue)
     } catch {
-      localStorage.setItem(key, JSON.stringify(value))
+      return value
     }
-  }, [key, value])
+  })
 
-  return [state, storeValue]
+  React.useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
+
+  return [state, setState]
 }
