d755746860ee8644e975eb576c7c37c1a13890de
SushanthK07 <sushanthkummitha@gmail.com>
2022-12-05 18:47:08 +0530

    usePersist hook

--- src/exercises/01-use-persist/hook.ts
+++ src/exercises/01-use-persist/hook.ts
@@ -1,1 +1,29 @@
-export const usePersist = () => null
+import * as React from 'react'
+
+export const usePersist = (
+  key: string,
+  value: unknown,
+): [unknown, (value: unknown) => void] => {
+  const [state, setState] = React.useState(value)
+
+  const storeValue = (value: unknown) => {
+    setState(value)
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  React.useEffect(() => {
+    try {
+      const storedValue = localStorage.getItem(key)
+      if (!storedValue) {
+        throw new Error()
+      }
+      const parsedValue = JSON.parse(storedValue)
+      setState(parsedValue)
+      localStorage.setItem(key, JSON.stringify(parsedValue))
+    } catch {
+      localStorage.setItem(key, JSON.stringify(value))
+    }
+  }, [key, value])
+
+  return [state, storeValue]
+}
