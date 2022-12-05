8058e17441b7a5cfba0917a2b222dd8c6870ce23
SushanthK07 <sushanthkummitha@gmail.com>
2022-12-05 21:03:36 +0530

    simpler dependancy

--- src/exercises/02-use-interval/hook.ts
+++ src/exercises/02-use-interval/hook.ts
@@ -1,23 +1,14 @@
 import * as React from 'react'
 
 export const useInterval = (callback: () => void, delay: number) => {
-  const [time, setTime] = React.useState(delay)
-
   const callbackRef = React.useRef(callback)
-  const intervalRef = React.useRef<ReturnType<typeof setInterval> | null>(null)
-
-  React.useEffect(() => {
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current)
-    }
-    setTime(delay)
-  }, [delay])
 
   React.useEffect(() => {
     callbackRef.current = callback
   }, [callback])
 
   React.useEffect(() => {
-    intervalRef.current = setInterval(() => callbackRef.current(), time)
-  }, [time])
+    const interval = setInterval(() => callbackRef.current(), delay)
+    return () => clearInterval(interval)
+  }, [delay])
 }
