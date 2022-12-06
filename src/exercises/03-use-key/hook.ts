870d30e33ec5d4311d794655b7649e94d802ddf0
SushanthK07 <sushanthkummitha@gmail.com>
2022-12-06 22:57:25 +0530

    useKey hook

--- src/exercises/03-use-key/hook.ts
+++ src/exercises/03-use-key/hook.ts
@@ -1,1 +1,25 @@
-export const useKey = () => null
+import * as React from 'react'
+
+export const useKey = (key: string, callback: () => void) => {
+  const callbackRef = React.useRef(callback)
+
+  React.useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
+
+  React.useEffect(() => {
+    const eventListener = (e: KeyboardEvent) => {
+      if (key === e.key) {
+        callbackRef.current()
+      }
+    }
+
+    // window.addEventListener('keydown', eventListener)
+    document.addEventListener('keydown', eventListener)
+
+    return () => {
+      // window.removeEventListener('keydown', eventListener)
+      document.removeEventListener('keydown', eventListener)
+    }
+  }, [key])
+}
