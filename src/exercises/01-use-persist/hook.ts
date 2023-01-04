d839a933c3b6c88c1cbdb42c548248c446e9b642
Meghana Varanasi <meghana.cosmos@gmail.com>
2023-01-04 21:50:50 +0530

    updated

--- src/exercises/01-use-persist/hook.ts
+++ src/exercises/01-use-persist/hook.ts
@@ -1,12 +1,28 @@
 import * as React from 'react'
 
-export const usePersist = <T>(key: string, initValue: T) => {
+export const usePersist = <T>(
+  key: string,
+  initValue: T,
+): [T, React.Dispatch<React.SetStateAction<T>>] => {
   const [value, setValue] = React.useState<T>(() => {
     const storedValue = window.localStorage.getItem(key)
-    return storedValue ? (JSON.parse(storedValue) as T) : initValue
+
+    if (storedValue) {
+      try {
+        return JSON.parse(storedValue) as T
+      } catch {
+        return initValue
+      }
+    }
+
+    return initValue
   })
+
   React.useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(value))
   }, [key, value])
   return [value, setValue]
 }
+
+// without return type - <T>(key: string, initValue: T) => (T | React.Dispatch<React.SetStateAction<T>>)[]
+// try if JSON.parse can be casted as T
