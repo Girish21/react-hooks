0240841adba479d872ce4c68be0498320fec5e41
Meghana Varanasi <meghana.cosmos@gmail.com>
2023-01-04 21:45:59 +0530

    01

--- src/exercises/01-use-persist/hook.ts
+++ src/exercises/01-use-persist/hook.ts
@@ -1,1 +1,12 @@
-export const usePersist = () => null
+import * as React from 'react'
+
+export const usePersist = <T>(key: string, initValue: T) => {
+  const [value, setValue] = React.useState<T>(() => {
+    const storedValue = window.localStorage.getItem(key)
+    return storedValue ? (JSON.parse(storedValue) as T) : initValue
+  })
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+  return [value, setValue]
+}
