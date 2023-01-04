2f88eeaaea5dfb46d3fa43ee8719ce40c633c585
Meghana Varanasi <meghana.cosmos@gmail.com>
2023-01-04 22:49:24 +0530

    useKey

--- src/exercises/03-use-key/hook.ts
+++ src/exercises/03-use-key/hook.ts
@@ -1,1 +1,17 @@
-export const useKey = () => null
+import * as React from 'react'
+
+export const useKey = (key: string, callback: () => void) => {
+  const callbackRef = React.useRef(callback)
+
+  React.useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
+
+  React.useEffect(() => {
+    document.addEventListener('keydown', event => {
+      if (event.key === key) {
+        callbackRef.current?.()
+      }
+    })
+  }, [key])
+}
